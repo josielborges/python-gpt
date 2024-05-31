10c936d725f43d5176b774a49415fe908a764568
Josiel <josiel.borges@sc.senai.br>
2024-05-31 09:47:26 -0300

    Improved categorizer code

--- categorizer.py
+++ categorizer.py
@@ -7,43 +7,49 @@ load_dotenv()
 openai = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
 model = "gpt-4"
 
-system_prompt = f"""
-        Você é um categorizador de produtos.
-        Você deve assumir as categorias presentes na lista abaixo.
 
-        # Lista de Categorias Válidas
-            - Moda Sustentável
-            - Produtos para o Lar
-            - Beleza Natural
-            - Eletrônicos Verdes
-            - Higiene Pessoal
+def categorize_product(product_name, categories):
+    system_prompt = f"""
+            Você é um categorizador de produtos.
+            Você deve assumir as categorias presentes na lista abaixo.
+    
+            # Lista de Categorias Válidas
+                {categories.split(',')}
+    
+            # Formato da Saída
+            Produto: Nome do Produto
+            Categoria: apresente a categoria do produto
+    
+            # Exemplo de Saída
+            Produto: Escova elétrica com recarga solar
+            Categoria: Eletrônicos Verdes
+        """
 
-        # Formato da Saída
-        Produto: Nome do Produto
-        Categoria: apresente a categoria do produto
+    response = openai.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt
+            },
+            {
+                "role": "user",
+                "content": product_name
+            }
+        ],
+        model=model,
+        temperature=0,
+        max_tokens=200
+    )
 
-        # Exemplo de Saída
-        Produto: Escova elétrica com recarga solar
-        Categoria: Eletrônicos Verdes
-    """
-
-user_prompt = input("Apresente o nome de um produto: ")
+    return response.choices[0].message.content
 
 
-response = openai.chat.completions.create(
-    messages=[
-        {
-            "role": "system",
-            "content": system_prompt
-        },
-        {
-            "role": "user",
-            "content": user_prompt
-        }
-    ],
-    model=model,
-    temperature=0,
-    max_tokens=200
-)
+categories = input("Informe o nome das categorias")
 
-print(response.choices[0].message.content)
+while (True):
+    product_name = input("Apresente o nome de um produto: ")
+    if product_name == '':
+        print("Encerrando o programa")
+        break
+    response = categorize_product(product_name, categories)
+    print(response)
